823af5dafb5cda9d692c078548f094a30937cb82
KozerenkoRoman <kozerenko@gmail.com>
2021-10-01 02:03:16 +0300

    feat: add dateSample.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function dateSample(sampleActivity) {
+    if (sampleActivity === "0") return false;
+    if (typeof sampleActivity != "string" || !isNaN(sampleActivity) && isNaN(parseInt(sampleActivity, 10)))
+        return false;
+
+    const decay = 0.693 / HALF_LIFE_PERIOD
+    const age = Math.ceil(1 / decay * Math.log(MODERN_ACTIVITY / Number(sampleActivity)));
+    return age > 0 && age
 }
+
