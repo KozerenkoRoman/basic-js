58e0c8ec0ef06f5d32ceed43e93edab519ee9360
KozerenkoRoman <kozerenko@gmail.com>
2021-10-03 20:43:22 +0300

    feat: add st-delete-digit.js

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
@@ -11,7 +9,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+    let min = String(n).split('').sort().slice(0, 1).join('');
+    let arr = String(n).split('');
+    let result = '';
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] == min) min = ''
+        else
+            result += arr[i];
+    }
+    return Number(result.split('').reverse().join(''));
 }
